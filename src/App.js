36b29549c84fca7576aa7a3c5bb7c14734d3c6ff
masukmia <dev.masuk@gmail.com>
2022-05-31 14:53:09 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Header from './components/Head/Header';
+import Hero from './components/Hero/Home';
+import Features from './components/Features/Features';
+import Portfolio from './components/Portfolio/Portfolio';
+import Resume from './components/Resume/Resume';
+import Testimonial from './components/Testimonial/Testimonial';
+import Blog from './components/Blog/Blog';
+import Contact from './components/Contact/Contact';
+import Footer from './components/Footer';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <Header />
+     <Hero />
+     <Features />
+     <Portfolio />
+     <Resume />
+      <Testimonial />
+      <Blog />
+      <Contact />
+      <Footer />
     </div>
   );
 }
